a2387df46ee4dab58f8605fdfe2e185b9be81f77
Eugene Krasnopolskiy <eugene.krasnopolskiy@gmail.com>
2017-08-14 16:21:26 +0300

    Added log activity events.

---
+++ app/src/main/java/com/studygoal/jisc/Managers/xApi/LogActivityEvent.java
@@ -0,0 +1,26 @@
+package com.studygoal.jisc.Managers.xApi;
+
+/**
+ * Created by Eugene Krasnopolskiy (eugene.krasnopolskiy@gmail.com) on 14.08.2017.
+ */
+public enum LogActivityEvent {
+    SuccessfulLogin,
+    NavigateActivityFeed,
+    NavigateFriends,
+    NavigateStatsAllActivity,
+    NavigateAttainment,
+    NavigatePoints,
+    NavigateLeaderboard,
+    NavigateEventsAttended,
+    NavigateAttendanceGraph,
+    NavigateLog,
+    NavigateTargetsMain,
+    NavigateTargetsGraphs,
+    ViewedGraphModule,
+    FilterLeaderboardByModule,
+    FilterEventsAttendedByModule,
+    FilterAttendanceGraph,
+    AddTimedLog,
+    AddReportedLog,
+    AddTarget
+}
--- app/src/main/java/com/studygoal/jisc/Managers/xApi/XApiManager.java
+++ app/src/main/java/com/studygoal/jisc/Managers/xApi/XApiManager.java
@@ -31,7 +31,98 @@ public class XApiManager {
         return sInstance;
     }
 
-    public void sendLogActivity(String verb) {
+    public void sendLogActivityEvent(LogActivityEvent event) {
+        sendLogActivityEvent(event, null);
+    }
+
+    public void sendLogActivityEvent(LogActivityEvent event, String modId) {
+        if (event != null) {
+            switch (event) {
+                case SuccessfulLogin: {
+                    sendLogActivity("loggedin");
+                    break;
+                }
+                case NavigateActivityFeed: {
+                    sendLogActivity("viewed", "feed-main", "MainFeed");
+                    break;
+                }
+                case NavigateFriends: {
+                    sendLogActivity("viewed", "feed-friends", "friends");
+                    break;
+                }
+                case NavigateStatsAllActivity: {
+                    sendLogActivity("viewed", "stats-main", "MainStats");
+                    break;
+                }
+                case NavigateAttainment: {
+                    sendLogActivity("viewed", "stats-attainment", "attainment");
+                    break;
+                }
+                case NavigatePoints: {
+                    sendLogActivity("viewed", "stats-points", "points");
+                    break;
+                }
+                case NavigateLeaderboard: {
+                    sendLogActivity("viewed", "stats-leaderboard", "leaderboard");
+                    break;
+                }
+                case NavigateEventsAttended: {
+                    sendLogActivity("viewed", "stats-events", "eventsAttended");
+                    break;
+                }
+                case NavigateAttendanceGraph: {
+                    sendLogActivity("viewed", "stats-attendance-summary", "attendanceGraph");
+                    break;
+                }
+                case NavigateLog: {
+                    sendLogActivity("viewed", "logs-main", "MainLogsPage");
+                    break;
+                }
+                case NavigateTargetsMain: {
+                    sendLogActivity("viewed", "targets-main", "MainTargetsPage");
+                    break;
+                }
+                case NavigateTargetsGraphs: {
+                    sendLogActivity("viewed", "targets-specific", "TargetsDonutCharts");
+                    break;
+                }
+                case ViewedGraphModule: {
+                    sendLogActivity("viewed", "stats-main-module", "MainStatsFilteredByModule");
+                    break;
+                }
+                case FilterLeaderboardByModule: {
+                    sendLogActivity("viewed", "stats-leaderboard-module", "leaderboardFilteredByModule", modId);
+                    break;
+                }
+                case FilterEventsAttendedByModule: {
+                    sendLogActivity("viewed", "stats-events-module", "eventsAttendedFilteredByModule", modId);
+                    break;
+                }
+                case FilterAttendanceGraph: {
+                    sendLogActivity("viewed", "stats-attendance-summary-module", "attendanceGraphFilteredByModule", modId);
+                    break;
+                }
+                case AddTimedLog: {
+                    sendLogActivity("viewed", "logs-timed", "logTimed", modId);
+                    break;
+                }
+                case AddReportedLog: {
+                    sendLogActivity("viewed", "logs-untimed", "logReport", modId);
+                    break;
+                }
+                case AddTarget: {
+                    if (modId == null) {
+                        sendLogActivity("viewed", "targets-add", "newTarget");
+                    } else {
+                        sendLogActivity("viewed", "targets-add", "newTarget", modId);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    private void sendLogActivity(String verb) {
         Observable<Boolean> observable = Observable.create(subscriber -> {
             boolean result = false;
 
@@ -62,7 +153,7 @@ public class XApiManager {
         observable.subscribeOn(Schedulers.io()).retry(1).subscribe();
     }
 
-    public void sendLogActivity(String verb, String contentId, String contentName) {
+    private void sendLogActivity(String verb, String contentId, String contentName) {
         Observable<Boolean> observable = Observable.create(subscriber -> {
             boolean result = false;
 
@@ -93,7 +184,7 @@ public class XApiManager {
         observable.subscribeOn(Schedulers.io()).retry(1).subscribe();
     }
 
-    public void sendLogActivity(String verb, String contentId, String contentName, String modId) {
+    private void sendLogActivity(String verb, String contentId, String contentName, String modId) {
         Observable<Boolean> observable = Observable.create(subscriber -> {
 
             boolean result = false;
