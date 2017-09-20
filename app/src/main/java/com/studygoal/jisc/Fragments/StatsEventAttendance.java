dca2a91bffadf30e5ba74330e140a07e07ba6d3f
Marjana-Tbox <31036857+Marjana-Tbox@users.noreply.github.com>
2017-09-20 17:50:42 +0100

    merge events attendance and attendance summary

--- app/src/main/java/com/studygoal/jisc/Fragments/StatsEventAttendance.java
+++ app/src/main/java/com/studygoal/jisc/Fragments/StatsEventAttendance.java
@@ -1,11 +1,25 @@
 package com.studygoal.jisc.Fragments;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.AbsListView;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import com.activeandroid.query.Select;
 import com.studygoal.jisc.Adapters.EventsAttendedAdapter;
@@ -16,6 +30,12 @@ import com.studygoal.jisc.Managers.xApi.XApiManager;
 import com.studygoal.jisc.Models.Event;
 import com.studygoal.jisc.R;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +50,17 @@ public class StatsEventAttendance extends BaseFragment {
     private ArrayList<Event> mEvents = new ArrayList<>();
     private boolean mIsLoading = false;
 
+    private WebView mWebView;
+    ArrayList<String> dates = new ArrayList<>();
+    ArrayList<String> count = new ArrayList<>();
+
+    private View mainView;
+    private boolean allSelected = true;
+    private TextView all;
+    private TextView summary;
+    private Switch viewSwitch;
+    private ViewFlipper viewFlipper;
+
     @Override
     public void onResume() {
         super.onResume();
@@ -42,11 +73,11 @@ public class StatsEventAttendance extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View mainView = inflater.inflate(R.layout.stats_event_attendance, container, false);
+        mainView = inflater.inflate(R.layout.stats_event_attendance, container, false);
         mAdapter = new EventsAttendedAdapter(getContext());
 
         mListView = (ListView) mainView.findViewById(R.id.event_attendance_listView);
-        LayoutInflater i = getActivity().getLayoutInflater();
+        //LayoutInflater inflater2 = getActivity().getLayoutInflater();
         mListView.setAdapter(mAdapter);
         mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
@@ -99,6 +130,45 @@ public class StatsEventAttendance extends BaseFragment {
             }
         }).start();
 
+        //1.4 feature
+        /*all = (TextView) mainView.findViewById(R.id.segment_button_all_events);
+        summary = (TextView) mainView.findViewById(R.id.segment_button_attendance_summary);
+
+        SegmentClickListener l = new SegmentClickListener();
+        all.setOnClickListener(l);
+        summary.setOnClickListener(l);
+
+        viewFlipper = (ViewFlipper) mainView.findViewById(R.id.viewFlipperEvents);
+
+        mWebView = (WebView) mainView.findViewById(R.id.webview_graph);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        try {
+            Log.e(getClass().getCanonicalName(), "attendance: " + preferences.getString(getString(R.string.attendance), null));
+            JSONArray jsonArray = new JSONArray(preferences.getString(getString(R.string.attendance), null));
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String[] dateinfo = jsonObject.getString("date").substring(0, 10).split("-");
+                String date = dateinfo[2]+ "/" + dateinfo[1];
+                dates.add(date);
+                count.add(jsonObject.getString("count"));
+                Log.e(dates.get(i), count.get(i));
+            }
+
+        } catch (Exception je) {
+            je.printStackTrace();
+        }
+
+        loadWebView();*/
+
         return mainView;
     }
 
@@ -109,4 +179,74 @@ public class StatsEventAttendance extends BaseFragment {
             mEvents.addAll(events);
         }
     }
+
+    //1.4 feature
+    /*@SuppressLint("SetJavaScriptEnabled")
+    private void loadWebView() {
+        WebSettings s = mWebView.getSettings();
+        s.setJavaScriptEnabled(true);
+
+        try {
+            InputStream is = getContext().getAssets().open("stats_attendance_high_chart.html");
+            int size = 0;
+            size = is.available();
+            final byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            mWebView.post(new Runnable() {
+                @Override
+                public void run() {
+                    double d = getActivity().getResources().getDisplayMetrics().density;
+                    int h = (int) (mWebView.getHeight() / d) - 20;
+                    int w = (int) (mWebView.getWidth() / d) - 20;
+
+                    String dataCount = "";
+                    String dataDate = "";
+                    for (int i = 0; i < dates.size(); i++) {
+                        dataCount += "" + count.get(i) + ", ";
+                        dataDate += "'" + dates.get(i) + "', \n";
+//                        data += "},";
+                    }
+
+
+                    String rawhtml = new String(buffer);
+                    rawhtml = rawhtml.replace("280px", w + "px");
+                    rawhtml = rawhtml.replace("220px", h + "px");
+                    rawhtml = rawhtml.replace("DATA", dataCount);
+                    rawhtml = rawhtml.replace("DATES", dataDate);
+                    mWebView.loadDataWithBaseURL("", rawhtml, "text/html", "UTF-8", "");
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class SegmentClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            allSelected = !allSelected;
+
+            if (allSelected) {
+                Drawable activeDrawable = ContextCompat.getDrawable(getContext(), R.drawable.round_corners_segmented_active);
+                all.setBackground(activeDrawable);
+                all.setTextColor(Color.WHITE);
+
+                summary.setBackground(null);
+                summary.setBackgroundColor(Color.TRANSPARENT);
+                summary.setTextColor(Color.parseColor("#3792ef"));
+                viewFlipper.showNext();
+            } else {
+                Drawable activeDrawable = ContextCompat.getDrawable(getContext(), R.drawable.round_corners_segmented_active_right);
+                summary.setBackground(activeDrawable);
+                summary.setTextColor(Color.WHITE);
+
+                all.setBackground(null);
+                all.setBackgroundColor(Color.TRANSPARENT);
+                all.setTextColor(Color.parseColor("#3792ef"));
+                viewFlipper.showPrevious();
+            }
+        }
+    }*/
 }
